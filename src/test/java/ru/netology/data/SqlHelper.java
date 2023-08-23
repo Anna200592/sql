72a27f6848a6a15613a9b0322bb796d9fe29f5cd
Neto test <strokova-92@mail.ru>
2023-08-23 11:40:26 +0300

    Initial commit

---
+++ src/test/java/ru/netology/data/SqlHelper.java
@@ -0,0 +1,37 @@
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class SqlHelper {
+    private static final QueryRunner runner = new QueryRunner();
+
+    private SqlHelper() {
+
+    }
+
+    private static Connection getConn() throws SQLException{
+        return DriverManager.getConnection(System.getProperty("db.url"),"app","pass");
+    }
+    @SneakyThrows
+    public static DataHelper.VerificationCode getVerificationCode(){
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
+        var connection = getConn();
+        var code = runner.query(connection, codeSQL, new ScalarHandler<String>());
+        return new DataHelper.VerificationCode(code);
+    }
+    @SneakyThrows
+    public static void cleanDB() {
+        var connection = getConn();
+        runner.update(connection, "DELETE FROM auth_codes");
+        runner.update(connection, "DELETE FROM card_transactions");
+        runner.update(connection, "DELETE FROM cards");
+        runner.update(connection, "DELETE FROM users");
+    }
+}
---
+++ src/test/java/ru/netology/test/DeadlineTests.java
@@ -0,0 +1,50 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ru.netology.data.DataHelper;
+import ru.netology.data.SqlHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SqlHelper.cleanDB;
+
+public class DeadlineTests {
+    @AfterAll
+    static void cleaner() {
+        cleanDB();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessFullLogin(){
+        var loginPage = open("http//localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyPageVisibility();
+        var verificationCode = SqlHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUser(){
+        var loginPage = open("http//localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyError();
+    }
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base and activ user and " +
+            "random verification code")
+    void shouldGetErrorNotificationIfLoginWithRandomUserVerificationCode(){
+        var loginPage = open("http//localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.getError();
+    }
+}
